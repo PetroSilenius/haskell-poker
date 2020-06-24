a1b07cb843fa4ad4a60aa17956c2f3d7df445a6e
Eero Ruohola <eero@ruohola.dev>
2020-06-24 19:57:13 +0300

    Add checking for flush, return numbers for hand ranks

--- Hands.hs
+++ Hands.hs
@@ -1,29 +1,60 @@
 module Hands where
 
-import Data.List ( groupBy, sortBy )
-import Data.Ord ( comparing )
-
 import Cards
+import Data.List (groupBy, sortBy)
+import Data.Ord (comparing)
 
 getGroups hand =
-    let equalRank = (\x y -> rank x == rank y)
-        groups = groupBy equalRank $ sortBy (comparing rank) hand in
-        reverse $ sortBy (comparing length) groups
+  let equalRank x y = rank x == rank y
+      groups = groupBy equalRank $ sortBy (comparing rank) hand
+   in reverse $ sortBy (comparing length) groups
 
-bestHand :: [Card] -> Maybe (String, [[Card]])
-bestHand hand = rankMatches hand
+getSuit :: Card -> Suit
+getSuit (Card r s) = s
 
-rankMatches :: [Card] -> Maybe (String, [[Card]])
-rankMatches hand =
-    let longest = getGroups hand !! 0
-        secondLongest = getGroups hand !! 1
-    in
-    case length longest of
-        4 -> Just ("Four of a kind", [longest])
+allEq :: [Card] -> Maybe Card -> Bool
+allEq (h:t) Nothing = allEq t (Just h)
+allEq (h:t) (Just e) = (getSuit h == getSuit e) && allEq t (Just e)
+allEq [] _ = True
+
+isFlush :: [Card] -> Bool
+isFlush hand = allEq hand Nothing
+
+isStraight :: [Card] -> Bool
+isStraight hands = False
+
+getHighestCard :: [Card] -> Card
+getHighestCard hand = last $ sortBy (comparing rank) hand
+
+getHandValue :: [Card] -> (Int, Card)
+getHandValue hand =
+  let longest = head (getGroups hand)
+      secondLongest = getGroups hand !! 1
+   in case length longest of
+        4 -> (8, head longest)
         3 -> case length secondLongest of
-            2 -> Just ("Full house", [longest, secondLongest])
-            _ -> Just ("Three of a kind", [longest])
+          2 -> (7, head secondLongest)
+          _ -> (4, head secondLongest)
         2 -> case length secondLongest of
-            2 -> Just ("Two pair", [longest, secondLongest])
-            _ -> Just ("Two of a kind", [longest])
-        _ -> Nothing
+          2 -> (3, head secondLongest)
+          _ -> (2, head secondLongest)
+        _ ->
+          if isFlush hand && isStraight hand
+            then (9, getHighestCard hand)
+            else
+              if isFlush hand
+                then (6, getHighestCard hand)
+                else (if isStraight hand then 5 else 1, getHighestCard hand)
+
+bestHand :: [Card] -> String
+bestHand hand =
+  case getHandValue hand of
+    (9, _) -> "Straight flush"
+    (8, _) -> "Four of a kind"
+    (7, _) -> "Full house"
+    (6, _) -> "Flush"
+    (5, _) -> "Straight"
+    (4, _) -> "Three of a kind"
+    (3, _) -> "Two of a kind"
+    (2, _) -> "Pair"
+    (1, _) -> "High card"
